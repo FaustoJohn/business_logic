c507b14edefc9adc278fffdbe21de71087d40fdd
Fausto Arcidiacono <fausto.arcidiacono@gmail.com>
2019-07-02 14:11:34 +0100

    .

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule BusinessLogic.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      test_coverage: [tool: ExCoveralls],
+      test_coverage: [tool: CoverModule],
       preferred_cli_env: [coveralls: :test],
       deps: deps()
     ]
